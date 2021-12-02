e829e4cbf5e7a83aa0004b1b126288bc2cf8e3f5
Ian Tu <ian_tu@hotmail.co.uk>
2021-12-02 05:17:37 +0000

    Day 2 Complete

--- Day02/main.jl
+++ Day02/main.jl
@@ -5,23 +5,62 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ cdeddb50-52d2-11ec-38a8-c5d56295952f
-md"""# Day  
+md"""# Day 2: Dive!
+
+Now, you need to figure out how to pilot this thing.
+
+It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
+
+forward X increases the horizontal position by X units.
+down X increases the depth by X units.
+up X decreases the depth by X units.
+Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+
+The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+
+```
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+```
+
+Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
+
+```
+forward 5 adds 5 to your horizontal position, a total of 5.
+down 5 adds 5 to your depth, resulting in a value of 5.
+forward 8 adds 8 to your horizontal position, a total of 13.
+up 3 decreases your depth by 3, resulting in a value of 2.
+down 8 adds 8 to your depth, resulting in a value of 10.
+forward 2 adds 2 to your horizontal position, a total of 15.
+After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+```
+
+Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
 
 """
 
 # ╔═╡ 1b840d5a-8b64-4b45-8537-849674f9c548
 begin		
-	println("\nDay ")
+	println("\nDay 2")
 	
 	script_dir = dirname(Base.@__FILE__)
 	input_file = joinpath(script_dir, "input.txt")
 
-	lines = Array{String}(undef, countlines(input_file))
+	num_lines = countlines(input_file)
+	commands = Array{String}(undef, num_lines)
+	values = Array{Int64}(undef, num_lines)
 
 	open(input_file) do f
 		current_index = 1
 		for l in eachline(f)
-        	lines[current_index] = l
+			command, value = split(l, " ")
+        	commands[current_index] = command
+			values[current_index] = parse(Int64, value)
+			#println(command, "   ", value)
 			current_index += 1
     	end
 	end	
@@ -29,19 +68,74 @@ end
 
 # ╔═╡ dfe034c8-820c-498b-a227-777515c4cee5
 begin		
-	println("Part 1: ", 0, "\n")
-	(0)
+	local horizontal_value = 0
+	local depth_value = 0
+	for (index, command) in enumerate(commands)
+		if command == "forward"
+			horizontal_value += values[index]
+		elseif command == "down"
+			depth_value += values[index]
+		else
+			depth_value -= values[index]
+		end		
+	end
+	println("Part 1: ", horizontal_value * depth_value)
+	Dict(["horizontal_value" => horizontal_value, 
+		"depth_value" => depth_value, 
+		"multiplied" => horizontal_value * depth_value])
 end
 
 # ╔═╡ 3d91d0d9-de7e-4aef-9e2d-0e03c96c31ad
 md"""
 
+Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+```
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+It increases your horizontal position by X units.
+It increases your depth by your aim multiplied by X.
+```
+
+Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+```
+forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+down 5 adds 5 to your aim, resulting in a value of 5.
+forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+up 3 decreases your aim by 3, resulting in a value of 2.
+down 8 adds 8 to your aim, resulting in a value of 10.
+forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+```
+
+Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
 """
 
 # ╔═╡ 3c9f2b64-b3af-4fa3-b668-0b57aabfad73
 begin		
-	println("Part 2: ", 0, "\n")
-	(0)
+	local horizontal_value = 0
+	local aim_value = 0
+	local depth_value = 0
+	for (index, command) in enumerate(commands)
+		if command == "forward"
+			horizontal_value += values[index]
+			depth_value += aim_value * values[index]
+		elseif command == "down"
+			aim_value += values[index]
+		else
+			aim_value -= values[index]
+		end		
+	end
+	println("Part 2: ", horizontal_value * depth_value, "\n")
+	Dict(["horizontal_value" => horizontal_value, 
+		"depth_value" => depth_value, 
+		"multiplied" => horizontal_value * depth_value])
 end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
