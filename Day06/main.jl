3a27b76deb035b122dd3095952917fb30fedd981
Ian Tu <ian_tu@hotmail.co.uk>
2021-12-06 12:38:26 +0000

    Completed Day 6

--- Day06/main.jl
+++ Day06/main.jl
@@ -5,43 +5,155 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ cdeddb50-52d2-11ec-38a8-c5d56295952f
-md"""# Day  
+md"""# Day 6: Lanternfish
 
+The sea floor is getting steeper. Maybe the sleigh keys got carried this way?
+
+A massive school of glowing lanternfish swims past. They must spawn quickly to reach such large numbers - maybe exponentially quickly? You should model their growth rate to be sure.
+
+Although you know nothing about this specific species of lanternfish, you make some guesses about their attributes. Surely, each lanternfish creates a new lanternfish once every 7 days.
+
+However, this process isn't necessarily synchronized between every lanternfish - one lanternfish might have 2 days left until it creates another lanternfish, while another might have 4. So, you can model each fish as a single number that represents the number of days until it creates a new lanternfish.
+
+Furthermore, you reason, a new lanternfish would surely need slightly longer before it's capable of producing more lanternfish: two more days for its first cycle.
+
+So, suppose you have a lanternfish with an internal timer value of 3:
+
+- After one day, its internal timer would become 2.
+- After another day, its internal timer would become 1.
+- After another day, its internal timer would become 0.
+- After another day, its internal timer would reset to 6, and it would create a new lanternfish with an internal timer of 8.
+- After another day, the first lanternfish would have an internal timer of 5, and the second lanternfish would have an internal timer of 7.
+
+A lanternfish that creates a new fish resets its timer to 6, not 7 (because 0 is included as a valid timer value). The new lanternfish starts with an internal timer of 8 and does not start counting down until the next day.
+
+Realizing what you're trying to do, the submarine automatically produces a list of the ages of several hundred nearby lanternfish (your puzzle input). For example, suppose you were given the following list:
+
+```
+3,4,3,1,2
+```
+
+This list means that the first fish has an internal timer of 3, the second fish has an internal timer of 4, and so on until the fifth fish, which has an internal timer of 2. Simulating these fish over several days would proceed as follows:
+
+```
+Initial state: 3,4,3,1,2
+After  1 day:  2,3,2,0,1
+After  2 days: 1,2,1,6,0,8
+After  3 days: 0,1,0,5,6,7,8
+After  4 days: 6,0,6,4,5,6,7,8,8
+After  5 days: 5,6,5,3,4,5,6,7,7,8
+After  6 days: 4,5,4,2,3,4,5,6,6,7
+After  7 days: 3,4,3,1,2,3,4,5,5,6
+After  8 days: 2,3,2,0,1,2,3,4,4,5
+After  9 days: 1,2,1,6,0,1,2,3,3,4,8
+After 10 days: 0,1,0,5,6,0,1,2,2,3,7,8
+After 11 days: 6,0,6,4,5,6,0,1,1,2,6,7,8,8,8
+After 12 days: 5,6,5,3,4,5,6,0,0,1,5,6,7,7,7,8,8
+After 13 days: 4,5,4,2,3,4,5,6,6,0,4,5,6,6,6,7,7,8,8
+After 14 days: 3,4,3,1,2,3,4,5,5,6,3,4,5,5,5,6,6,7,7,8
+After 15 days: 2,3,2,0,1,2,3,4,4,5,2,3,4,4,4,5,5,6,6,7
+After 16 days: 1,2,1,6,0,1,2,3,3,4,1,2,3,3,3,4,4,5,5,6,8
+After 17 days: 0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8
+After 18 days: 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
+```
+
+Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each other number decreases by 1 if it was present at the start of the day.
+
+In this example, after 18 days, there are a total of 26 fish. After 80 days, there would be a total of `5934`.
+
+Find a way to simulate lanternfish. **How many lanternfish would there be after 80 days?**
 """
 
 # ╔═╡ 1b840d5a-8b64-4b45-8537-849674f9c548
 begin		
-	println("\nDay ")
+	println("\nDay 6: Lanternfish")
 	
 	script_dir = dirname(Base.@__FILE__)
 	input_file = joinpath(script_dir, "input.txt")
 
-	lines = Array{String}(undef, countlines(input_file))
+	initial_state = Vector{Int}(undef, 0)
 
 	open(input_file) do f
-		current_index = 1
 		for l in eachline(f)
-        	lines[current_index] = l
-			current_index += 1
+        	num_str_vector = split(l, ",")		
+			resize!(initial_state, length(num_str_vector))	
+			for (index, num_str) in enumerate(num_str_vector)
+				num = parse(Int, num_str)
+				initial_state[index] = num
+			end
     	end
-	end	
+	end
 end
 
 # ╔═╡ dfe034c8-820c-498b-a227-777515c4cee5
 begin		
-	println("Part 1: ", 0, "\n")
-	(0)
+	local initial_state_copy = deepcopy(initial_state)
+	local current_size = length(initial_state)
+	local day_dict = Dict(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0)
+
+	for num in initial_state_copy
+		day_dict[num] += 1
+	end
+
+	for day in 1:80
+		current_day_0_count = day_dict[0]
+
+		for day_key in 1:8
+			day_dict[day_key-1] = day_dict[day_key]
+		end
+
+		day_dict[6] += current_day_0_count
+		day_dict[8] = current_day_0_count
+	end
+
+	local total = 0
+	for val in values(day_dict)
+		total += val
+	end
+
+	println("Part 1: ", total)
+	(total)
 end
 
 # ╔═╡ 3d91d0d9-de7e-4aef-9e2d-0e03c96c31ad
 md"""
 
+Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+After 256 days in the example above, there would be a total of `26984457539` lanternfish!
+
+**How many lanternfish would there be after 256 days?**
+
 """
 
 # ╔═╡ 3c9f2b64-b3af-4fa3-b668-0b57aabfad73
 begin		
-	println("Part 2: ", 0, "\n")
-	(0)
+	local initial_state_copy = deepcopy(initial_state)
+	local current_size = length(initial_state)
+	local day_dict = Dict(0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0)
+
+	for num in initial_state_copy
+		day_dict[num] += 1
+	end
+
+	for day in 1:256
+		current_day_0_count = day_dict[0]
+
+		for day_key in 1:8
+			day_dict[day_key-1] = day_dict[day_key]
+		end
+
+		day_dict[6] += current_day_0_count
+		day_dict[8] = current_day_0_count
+	end
+
+	local total = 0
+	for val in values(day_dict)
+		total += val
+	end
+
+	println("Part 2: ", total, "\n")
+	(total)
 end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
